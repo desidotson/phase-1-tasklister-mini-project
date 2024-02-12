fd496d5a60b0bb8a1b96d6fc9bfa10d5fb256b15
Desmonne Dotson <brazette97@gmail.com>
2024-02-12 17:20:09 -0600

    mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector('form'); 
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const taskDescription = document.querySelector("#new-task-description");
+    handleTask(taskDescription.value);
+  })
+})
+
+function handleTask(task){
+  const li = document.createElement('li')
+  const btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'remove'
+  li.textContent = task
+  li.appendChild(btn)
+
+  document.querySelector("#tasks").appendChild(li)
+}
+
+function handleDelete (e){
+   e.target.parentNode.remove()
+}
